8bc202929d0aa6caae95b800ebc755b262b84c27
GitHub Action <action@github.com>
2026-06-28 11:22:17 +0000

    Initialize template repository proper
    Set package name to com.github.kamineq.forestclient
    Set project name to ForestClient
    Set modid to forestclient

--- src/main/java/com/github/kamineq/forestclient/ExampleMod.java
+++ src/main/java/com/github/kamineq/forestclient/ExampleMod.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.github.kamineq.forestclient;
 
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "forestclient", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
